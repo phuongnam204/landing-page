5dc57de1c019def9cf0111e7fca136348421a120
Nam <thecallingspectre@gmail.com>
2026-07-02 13:17:56 +0700

    chore: mark AppFlow as a client component for Next.js

--- src/components/AppFlow.tsx
+++ src/components/AppFlow.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState, useEffect, useRef } from 'react';
 import { quizQuestions, q6Options } from '../content/quiz';
 import type { QuizResult, ProgramId } from '../content/quiz';
